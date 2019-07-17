a45e85d339a4b02dd8e186b032d1756df5095b0e
Ubuntu <ubuntu@ip-172-31-92-163.ec2.internal>
2019-07-17 21:27:23 +0000

    fixed uncaught errors

--- js/index.js
+++ js/index.js
@@ -1,40 +1,131 @@
-let order = [];
-let playerOrder = [];
-let flash;
-let turn;
-let good;
-let compTurn;
-let intervalId;
-let strict = false;
-let noise = true;
-let on = false;
-let win;
+    let order = [];
+    let playerOrder = [];
+    let flash;
+    let turn;
+    let good;
+    let compTurn;
+    let intervalId;
+    let strict = false;
+    let noise = true;
+    let on = false;
+    let win;
 
-const turnCounter = document.querySelector("#turn"); 
-const topLeft = document.querySelector("#topLeft"); 
-const topRight = document.querySelector("#topRight"); 
-const bottomLeft = document.querySelector("#bottomLeft"); 
-const bottomRight = document.querySelector("#bottomRight"); 
-const strictButton = document.querySelector("#strictButton"); 
-const onButton = document.querySelector("#onButton"); 
-const startButton = document.querySelector("#startButton"); 
+    const turnCounter = document.querySelector("#turn");
+    const topLeft = document.querySelector("#topLeft");
+    const topRight = document.querySelector("#topRight");
+    const bottomLeft = document.querySelector("#bottomLeft");
+    const bottomRight = document.querySelector("#bottomRight");
+    const strictButton = document.querySelector("#strict");
+    const onButton = document.querySelector("#on");
+    const startButton = document.querySelector("#start");
 
-strictButton.addEventListener('change', (event) => {
-    if(strictButton.checked == true){
-        strict = true;
-    }
-    else{
-        strict = false;
-    }
-});
+    strictButton.addEventListener('change', (event) => {
+        if (strictButton.checked == true) {
+            strict = true;
+        }
+        else {
+            strict = false;
+        }
+    });
 
-onButton.addEventListener('change', (event) => {
-    if(onButton.checked == true){
-        on = true;
-        turnCounter.innerHTML="-";
+    onButton.addEventListener('change', (event) => {
+        if (onButton.checked == true) {
+            on = true;
+            turnCounter.innerHTML = "-";
+        }
+        else {
+            on = false;
+            turnCounter.innerHTML = "";
+            clearColor();
+            clearInterval(intervalId);
+        }
+    });
+
+
+    startButton.addEventListener('click', (event) => {
+        if (on || win) {
+            play();
+        }
+    });
+
+    function play() {
+        win = false;
+        order = [];
+        playerOrder = [];
+        flash = 0;
+        intervalId = 0;
+        turn = 1;
+        turnCounter.innerHTML = 1;
+        good = true;
+
+        for (var i = 0; i < 20; i++) {
+            order.push(Math.floor(Math.random() * 4) + 1)
+        }
+        compTurn = true;
+
+        intervalId = setInterval(gameTurn, 800)
     }
-    else{
+
+    function gameTurn() {
         on = false;
-    }
-});
 
+        if (flash == turn) {
+            clearInterval(intervalId);
+            compTurn = false;
+            clearColor();
+            on = true;
+        }
+        if (compTurn) {
+            clearColor();
+            setTimeout(() => {
+                if (order[Flash] == 1) one();
+                if (order[Flash] == 2) two();
+                if (order[Flash] == 3) three();
+                if (order[Flash] == 4) four();
+                flash++;
+            }, 200);
+        }
+    }
+
+    function one() {
+        if (noise) {
+            let audio = document.getElementById("clip1");
+            audio.play();
+        }
+        noise = true;
+        topLeft.style.backgroundColor = "lightgreen";
+    }
+
+    function two() {
+        if (noise) {
+            let audio = document.getElementById("clip2");
+            audio.play();
+        }
+        noise = true;
+        topRight.style.backgroundColor = "tomato";
+    }
+
+    function three() {
+        if (noise) {
+            let audio = document.getElementById("clip3");
+            audio.play();
+        }
+        noise = true;
+        bottomLeft.style.backgroundColor = "yellow";
+    }
+
+    function four() {
+        if (noise) {
+            let audio = document.getElementById("clip4");
+            audio.play();
+        }
+        noise = true;
+        bottomRight.style.backgroundColor = "lightskyblue";
+    }
+
+    function clearColor() {
+        topLeft.style.backgroundColor = "darkgreen";
+        topRight.style.backgroundColor = "darkred";
+        bottomLeft.style.backgroundColor = "goldenrod";
+        bottomRight.style.backgroundColor = "darkblue";
+    }
